77b5953a48c3e7f59886838f39f9d3bfedc009c2
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-11 21:10:00 +0530

    backend created

---
+++ FitnessTrackerServer/src/test/java/com/devstack/FitnessTrackerServer/FitnessTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstack.FitnessTrackerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
