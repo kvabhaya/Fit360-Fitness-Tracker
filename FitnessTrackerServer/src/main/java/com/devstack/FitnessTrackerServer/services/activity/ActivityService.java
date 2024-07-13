8bf793c821aec63aff08c6de4053ffe9a084e896
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-13 23:07:19 +0530

    update Activity Service

--- FitnessTrackerServer/src/main/java/com/devstack/FitnessTrackerServer/services/activity/ActivityService.java
+++ FitnessTrackerServer/src/main/java/com/devstack/FitnessTrackerServer/services/activity/ActivityService.java
@@ -1,4 +1,8 @@
 package com.devstack.FitnessTrackerServer.services.activity;
 
+import com.devstack.FitnessTrackerServer.dto.ActivityDto;
+
 public interface ActivityService {
+
+    ActivityDto postActivity(ActivityDto dto);
 }
