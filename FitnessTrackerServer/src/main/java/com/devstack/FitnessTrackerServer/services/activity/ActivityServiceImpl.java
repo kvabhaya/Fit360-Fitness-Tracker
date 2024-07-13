0c9501b8c3e3c24e53b8a80426308daf4c934f7d
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-13 23:06:14 +0530

    update Activity Service Implementation

--- FitnessTrackerServer/src/main/java/com/devstack/FitnessTrackerServer/services/activity/ActivityServiceImpl.java
+++ FitnessTrackerServer/src/main/java/com/devstack/FitnessTrackerServer/services/activity/ActivityServiceImpl.java
@@ -1,7 +1,25 @@
 package com.devstack.FitnessTrackerServer.services.activity;
 
+import com.devstack.FitnessTrackerServer.Entity.Activity;
+import com.devstack.FitnessTrackerServer.Repository.ActivityRepository;
+import com.devstack.FitnessTrackerServer.dto.ActivityDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ActivityServiceImpl {
+@RequiredArgsConstructor
+public class ActivityServiceImpl implements ActivityService{
+    private final ActivityRepository activityRepository;
+    private final ActivityRepository;
+
+    public ActivityDto postActivity(ActivityDto dto){
+        Activity activity = new Activity();
+
+        activity.setDate(dto.getDate());
+        activity.setSteps(dto.getSteps());
+        activity.setDistance(dto.getDistance());
+        activity.setCaloriesBurned(dto.getCaloriesBurned());
+
+        return activityRepository.save(activity).getActivityDto();
+    }
 }
