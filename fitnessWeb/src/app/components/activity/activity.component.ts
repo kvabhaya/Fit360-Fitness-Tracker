afdf191c562118642c9880529ec0e04d3da914b9
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-28 10:26:52 +0530

    updated activity.component.ts

--- fitnessWeb/src/app/components/activity/activity.component.ts
+++ fitnessWeb/src/app/components/activity/activity.component.ts
@@ -3,10 +3,11 @@ import {NzCardComponent, NzCardGridDirective} from "ng-zorro-antd/card";
 import {NgStyle} from "@angular/common";
 import {NzColDirective} from "ng-zorro-antd/grid";
 import {NzFormControlComponent, NzFormDirective, NzFormItemComponent} from "ng-zorro-antd/form";
-import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NzInputDirective} from "ng-zorro-antd/input";
 import {NzDatePickerComponent} from "ng-zorro-antd/date-picker";
 import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
   selector: 'app-activity',
@@ -28,5 +29,20 @@ import {NzButtonComponent} from "ng-zorro-antd/button";
   styleUrl: './activity.component.scss'
 })
 export class ActivityComponent {
-
+  gridStyle = {
+    width:'100%',
+    textAlign: 'center'
+  };
+  activityForm!: FormGroup;
+  constructor(private fb:FormBuilder,
+              private message: NzMessageService
+  ) {}
+  ngOnInit(){
+    this.activityForm = this.fb.group({
+      caloriesBurned:[null, [Validators.required]],
+      steps:[null, [Validators.required]],
+      distance:[null, [Validators.required]],
+      date:[null, [Validators.required]],
+    })
+  }
 }
