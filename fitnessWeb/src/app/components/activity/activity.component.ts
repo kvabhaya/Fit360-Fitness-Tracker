386c7fec73268fa4205544615f997548695a93a1
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-28 10:11:44 +0530

    updated activity.components.html

--- fitnessWeb/src/app/components/activity/activity.component.ts
+++ fitnessWeb/src/app/components/activity/activity.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {NzCardComponent, NzCardGridDirective} from "ng-zorro-antd/card";
+import {NgStyle} from "@angular/common";
+import {NzColDirective} from "ng-zorro-antd/grid";
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent} from "ng-zorro-antd/form";
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {NzInputDirective} from "ng-zorro-antd/input";
+import {NzDatePickerComponent} from "ng-zorro-antd/date-picker";
 
 @Component({
   selector: 'app-activity',
   standalone: true,
-  imports: [],
+  imports: [
+    NzCardGridDirective,
+    NgStyle,
+    NzColDirective,
+    NzCardComponent,
+    NzFormDirective,
+    ReactiveFormsModule,
+    NzFormItemComponent,
+    NzFormControlComponent,
+    NzInputDirective,
+    NzDatePickerComponent
+  ],
   templateUrl: './activity.component.html',
   styleUrl: './activity.component.scss'
 })
